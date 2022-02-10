6b3157da362e3758cf34d65bb635b5740724372c
Philippe Allard-Rousse <philrousse@gmail.com>
2022-02-10 14:51:16 -0500

    Remove AuthGuard for developpement

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthComponent } from './auth/auth.component';
+import { WeatherComponent } from './weather/weather.component';
 
 const routes: Routes = [
   //On redirige / vers /weather
@@ -11,7 +12,7 @@ const routes: Routes = [
   //canActivate: [AuthGuard] permet de bloquer 
   //une route si l'utilisateur n'est pas login
   //TODO vous pouvez retirer temporairement le guard pour développer
-  {path:'weather', component: AppComponent, canActivate: [AuthGuard]},
+  {path:'weather', component: WeatherComponent/* , canActivate: [AuthGuard] */},
 
   //Route pour la page d'authentification
   {path:'auth', component: AuthComponent },
